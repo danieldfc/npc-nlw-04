d6000b5cdd2f87b65b134ecc4291425f9412a021
Daniel Felizardo <daniel.david772@gmail.com>
2021-02-24 18:54:42 -0300

    primeira rota test

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.get('/users', (reques, response) => {
+  return response.json({ mensagem: "Hello World - NLW04" });
+});
+
 app.listen(3333, () => console.log('> Server is starting...'));
